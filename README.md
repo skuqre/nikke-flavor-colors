186e758f31bb2d7256154e2a366e0928f97423ab
skuqre <74248935+skuqre@users.noreply.github.com>
2024-02-04 16:11:09 +0800

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# NIKKE flavor colors
+
+a Python script that lets you automagically capture all playable nikke's "flavor" colors.
+
+## How?
+
+0. Install requirements. There are probably some missing but I think that's it
+1. Open NIKKE. (be sure to close the launcher to prevent window name confusion!)
+    - Don't be on full screen, you're gonna need multiple windows up
+    - I'd recommend NIKKE to atleast take up all of your desktop's height. The text recognition part for the name may mess up on smaller window sizes.
+2. Head to the Nikkepedia, and select whatever Nikke you want to start with. You can just choose Emma as well.
+    - When on a [NIKKE's profile screen](https://haxeflixel.is-terrible.com/6reOaDFj0.png), be sure that the [main part of the profile](https://haxeflixel.is-terrible.com/6reOlPatG.png) **isn't obstructed in any way**. If there is a window on top of the NIKKE window, the script won't be able to capture it.
+    - Also, do not obstruct the [right arrow](https://haxeflixel.is-terrible.com/6reO_hPUh.png) as that will be clicked by your mouse to go to the next NIKKE.
+3. Open an administrator Command Prompt. Just look up `cmd` and click "run as administrator" when it's on the side of the start menu.
+4. Run `main.py` in the said administrator command prompt. Then:
+    - Press enter.
+    - **Immediately after pressing enter, click the NIKKE window immediately.**
+5. Let it run it's course, it's going to stop immediately when it comes back to the Nikke you've first selected.
+    - It creates 2 images: `color.png` and `name.png`, both containing screenshots of the screen.
+    - It also creates `colors.json`, which is the dictionary of colors, with the key being the formatted name of the Nikke (e.g. `Anis: Sparkling Summer`) and the value being the hex code of the color.
+
+## Warnings
+
+**This *may* count as a macro software**, so NIKKE *may* ban you. I'm not entirely sure yet, but be on the lookout. <u>Use at your own risk</u>.
+
+**The color may be ever-changing when rerunning it again**. NIKKE runs a weird image filter over everything, so colors may vary every once in a while.
---
+++ main.py
@@ -0,0 +1,83 @@
+from PIL import Image
+import pyscreenshot as pss
+import pygetwindow as pgw
+import pytesseract
+import os
+import pyautogui as pag
+from dotenv import load_dotenv
+from time import sleep
+import win32gui
+import cv2
+import json
+
+load_dotenv()
+
+WINDOW_NAME = "NIKKE"
+
+pytesseract.pytesseract.tesseract_cmd = os.getenv('TESSERACT_LOCATION')
+
+def main():
+    FIRST_NIKKE = None
+    SECONDS_BEFORE_START = 5
+
+    color_table = {}
+
+    while SECONDS_BEFORE_START > 0:
+        print("Starting in " + str(SECONDS_BEFORE_START) + " seconds... Click on NIKKE now and wait for me to start...")
+        SECONDS_BEFORE_START -= 1
+        sleep(1);
+
+    while True:
+
+        # get the window
+        window = pgw.getWindowsWithTitle(WINDOW_NAME)[0]
+
+        # get the window coordinates
+        x, y, w, h = window.left, window.top, window.width, window.height
+
+        # box coords
+        bx, by, bw, bh = (530/1452) * w, (550/817) * h, (w / 1452) * 190, (h / 817) * 39
+
+        # final coords
+        x1, y1, x2, y2 = x + bx, y + by, x + bx + bw, y + by + bh
+
+        ss = pss.grab(bbox=(round(x1), round(y1), round(x2), round(y2)))
+        ss.save(__file__ + '\..\\name.png')
+
+        # perform edits
+        img = cv2.imread((__file__ + '\..\\name.png'));
+        edit = cv2.convertScaleAbs(img, alpha=1.5, beta=5)
+        cv2.imwrite((__file__ + '\..\\name.png'), edit)
+
+        # color coords
+        cx, cy, cw, ch = (724/1452) * w, (568/817) * h, (w / 1452) * 10, (h / 817) * 10
+
+        cs = pss.grab(bbox=(round(x + cx), round(y + cy), round(x + cx + cw), round(y + cy + ch)))
+        cs.save(__file__ + '\..\\color.png')
+
+        # get color
+        pixels = Image.open(__file__ + '\..\\color.png').load()
+
+        text = pytesseract.image_to_string(Image.open(__file__ + '\..\\name.png'), config='--psm 7')
+        print("WHO: " + text, "COLOR: " + str(pixels[0, 0]))
+
+        if text == FIRST_NIKKE:
+            break
+        else:
+            color_table[text.strip()] = '#%02x%02x%02x' % pixels[0, 0]
+
+        if FIRST_NIKKE == None:
+            FIRST_NIKKE = text
+
+        win32gui.SetForegroundWindow(win32gui.FindWindow(None, WINDOW_NAME))
+        pag.click(x + w - 32, y + round(h / 2) + 32, button='left')
+
+        with open(__file__ + '\..\\colors.json', 'w') as f:
+            json.dump(color_table, f, indent='\t')
+
+        sleep(0.1)
+
+    print(color_table)
+
+if __name__ == "__main__":
+    main()
